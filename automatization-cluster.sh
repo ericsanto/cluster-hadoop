56222ab05f3279ebf628aaca2ba13a4a2b02d3a2
eric <ericjesus403@gmail.com>
2024-08-26 09:58:29 -0300

    Novas configurações em yarn, mapred, core-site e hdfs-site

--- automatization-cluster.sh
+++ automatization-cluster.sh
@@ -25,7 +25,7 @@ lines_to_add=$(cat <<- EOM
 export JAVA_HOME=/usr/lib/jvm/jdk-22.0.2-oracle-x64
 export PATH=\$PATH:\$JAVA_HOME/bin
 export HADOOP_HOME="/home/hadoop/hadoop"
-export PATH="\$PATH:\${HADOOP_HOME}/bin
+export PATH="\$PATH:\${HADOOP_HOME}/bin"
 EOM
 )
 
@@ -58,15 +58,17 @@ path_to_add_config_core_site_xml="$path_files_hadoop/core-site.xml"
 lines_to_add_in_hadoop_core_site_xml=$(cat <<- EOM
 <configuration>
 
-<property>
+    <property>
 
-<name>fs.default.name</name>
+        <name>fs.default.name</name>
 
-<value>hdfs://master:19000</value>
+        <value>hdfs://master:19000</value>
 
-</property>
+    </property>
 
 </configuration>
+
+image
 EOM
 )
 
@@ -88,33 +90,33 @@ mkdir -p "$HOME/hadoop/dfs/namespace_logs"
 # Caminho para o arquivo hdfs-site.xml
 path_to_add_config_hdfs_site_xml="$path_files_hadoop/hdfs-site.xml"
 lines_to_add_in_config_hdfs_site_xml=$(cat <<- EOM
-    <configuration>
+<configuration>
 
-     <property>
+    <property>
 
-     <name>dfs.replication</name>
+        <name>dfs.replication</name>
 
-     <value>3</value>
+        <value>3</value>
 
-     </property>
+    </property>
 
-     <property>
+    <property>
 
-     <name>dfs.namenode.name.dir</name>
+        <name>dfs.namenode.name.dir</name>
 
-     <value>/home/hadoop/hadoop/dfs/namespace_logs</value>
+        <value>/home/hadoop/hadoop/dfs/namespace_logs</value>
 
-     </property>
+    </property>
 
-     <property>
+    <property>
 
-     <name>dfs.datanode.data.dir</name>
+        <name>dfs.datanode.data.dir</name>
 
-     <value>/home/hadoop/hadoop/dfs/data</value>
+        <value>/home/hadoop/hadoop/dfs/data</value>
 
-     </property>
+    </property>
 
-    </configuration>
+</configuration>
 EOM
 )
 
@@ -131,59 +133,59 @@ echo "Linhas adicionadas ao $path_to_add_config_hdfs_site_xml com sucesso!"
 # Caminho para o arquivo mapred-site.xml
 path_to_add_in_config_map_reduce_xml="$path_files_hadoop/mapred-site.xml"
 lines_to_add_in_config_mapred_site_xml=$(cat <<- EOM
-    <configuration>
+<configuration>
 
-     <property>
+    <property>
 
-     <name>mapreduce.job.user.name</name>
+        <name>mapreduce.job.user.name</name>
 
-     <value>hadoop</value>
+        <value>hadoop</value>
 
-     </property>
+    </property>
 
      
 
-     <property>
+    <property>
 
-     <name>yarn.resourcemanager.address</name>
+        <name>yarn.resourcemanager.address</name>
 
-     <value>master:8032</value>
+        <value>master:8032</value>
 
-     </property>
+    </property>
 
     <property> 
 
-     <name>mapreduce.framework.name</name> 
+        <name>mapreduce.framework.name</name> 
 
-     <value>yarn</value> 
+        <value>yarn</value> 
 
-     </property>
-
-    <property>
-
-     <name>yarn.app.mapreduce.am.env</name>
-
-     <value>HADOOP_MAPRED_HOME=/home/hadoop/hadoop</value>
-
-     </property>
+    </property>
 
     <property>
 
-     <name>mapreduce.map.env</name>
+        <name>yarn.app.mapreduce.am.env</name>
 
-     <value>HADOOP_MAPRED_HOME=/home/hadoop/hadoop</value>
+        <value>HADOOP_MAPRED_HOME=/home/hadoop/hadoop</value>
 
-     </property>
+    </property>
 
     <property>
 
-     <name>mapreduce.reduce.env</name>
+        <name>mapreduce.map.env</name>
 
-     <value>HADOOP_MAPRED_HOME=/home/hadoop/hadoop</value>
+        <value>HADOOP_MAPRED_HOME=/home/hadoop/hadoop</value>
 
-     </property>
+    </property>
 
-    </configuration>
+    <property>
+
+        <name>mapreduce.reduce.env</name>
+
+        <value>HADOOP_MAPRED_HOME=/home/hadoop/hadoop</value>
+
+    </property>
+
+</configuration>
 EOM
 )
 
@@ -200,99 +202,89 @@ echo "Linhas adicionadas ao $path_to_add_in_config_map_reduce_xml com sucesso!"
 # Caminho para o arquivo yarn-site.xml
 path_to_add_in_config_yarn_site_xml="$path_files_hadoop/yarn-site.xml"
 line_to_add_in_config_yarn_site_xml=$(cat <<- EOM
-    <configuration>
+<configuration>
 
     <property>
 
-     <name>yarn.resourcemanager.hostname</name>
+        <name>yarn.resourcemanager.hostname</name>
 
-     <value>master</value>
+        <value>master</value>
 
-     </property>
-
-    <property>
-
-     <name>yarn.nodemanager.resource.memory-mb</name>
-
-     <value>1536</value>
-
-     </property>
+    </property>
 
     <property>
 
-     <name>yarn.scheduler.maximum-allocation-mb</name>
+        <name>yarn.nodemanager.resource.memory-mb</name>
 
-     <value>1536</value>
+        <value>1536</value>
 
-     </property>
-
-    <property>
-
-     <name>yarn.scheduler.minimum-allocation-mb</name>
-
-     <value>128</value>
-
-     </property>
+    </property>
 
     <property>
 
-     <name>yarn.nodemanager.vmem-check-enabled</name>
+        <name>yarn.scheduler.maximum-allocation-mb</name>
 
-     <value>false</value>
+        <value>1536</value>
 
-     </property>
-
-    <property>
-
-     <name>yarn.server.resourcemanager.application.expiry.interval</name>
-
-     <value>60000</value>
-
-     </property>
+    </property>
 
     <property>
 
-     <name>yarn.nodemanager.aux-services</name>
+        <name>yarn.scheduler.minimum-allocation-mb</name>
 
-     <value>mapreduce_shuffle</value>
+        <value>128</value>
 
-     </property>
-
-    <property>
-
-     <name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>
-
-     <value>org.apache.hadoop.mapred.ShuffleHandler</value>
-
-     </property>
+    </property>
 
     <property>
 
-     <name>yarn.log-aggregation-enable</name>
+        <name>yarn.nodemanager.vmem-check-enabled</name>
 
-     <value>true</value>
+        <value>false</value>
 
-     </property>
-
-    <property>
-
-     <name>yarn.log-aggregation.retain-seconds</name>
-
-     <value>-1</value>
-
-     </property>
+    </property>
 
     <property>
 
-     <name>yarn.application.classpath</name>
+        <name>yarn.server.resourcemanager.application.expiry.interval</name>
 
-     <value>\$HADOOP_CONF_DIR,\${HADOOP_COMMON_HOME}/share/hadoop/common/*,\${HADOOP_COMMON_HOME}/share/hadoop/common/lib/*,\${HADOOP_HDFS_HOME}/share/hadoop/hdfs/*,\${HADOOP_HDFS_HOME}/share/hadoop/hdfs/lib/*,\${HADOOP_MAPRED_HOME}/share/hadoop/mapreduce/*,\${HADOOP_MAPRED_HOME}/share/hadoop/mapreduce/lib/*,\${HADOOP_YARN_HOME}/share/hadoop/yarn/*,\${HADOOP_YARN_HOME}/share/hadoop/yarn/lib/*</value>
+        <value>60000</value>
 
-     </property>
+    </property>
 
-     
+    <property>
 
-    </configuration
+        <name>yarn.nodemanager.aux-services</name>
+
+        <value>mapreduce_shuffle</value>
+
+    </property>
+
+    <property>
+
+        <name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>
+
+        <value>org.apache.hadoop.mapred.ShuffleHandler</value>
+
+    </property>
+
+    <property>
+
+        <name>yarn.log-aggregation-enable</name>
+
+        <value>true</value>
+
+    </property>
+
+    <property>
+
+        <name>yarn.log-aggregation.retain-seconds</name>
+
+        <value>-1</value>
+
+    </property>
+
+</configuration>
 EOM
 )
 
