fce7cc24a9ece9547045f1e35e6b4be73f02bf59
eric <ericjesus403@gmail.com>
2024-08-29 16:49:10 -0300

    Instação e configuração do zabbix

--- script-database-zabbix.sh
+++ script-database-zabbix.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+echo "BEM VINDO AO CLUSTER APACHE HADOOP"
+echo "Aguarde 10 segundos. Estamos preparando o melhor ambiente para você :)"
+
+sleep 10
+
+path_file_locale="/etc/default/locale"
+
+sudo locale-gen pt_BR.UTF-8
+sudo update-locale LANG=pt_BR.UTF-8
+echo "LANG=pt_BR.UTF-8"  | sudo tee "$path_file_locale"
+echo "LANGUAGE=pt_BR:pt" | sudo tee "$path_file_locale"
+echo "LC_ALL=pt_BR.UTF-8" | sudo tee "$path_file_locale"
+
 MYSQL_PASSWORD_ROOT="default"
 ZABBIX_DB="zabbix"
 ZABBIX_USER="zabbix"
 ZABBIX_PASSWORD="zabbix_password"
 SQL_SCRIPT_PATH="/usr/share/zabbix-sql-scripts/mysql/server.sql.gz"
 
-mysql -u root -p"$MYSQL_PASSWORD_ROOT" <<EOF
+USER_HADOOP="hadoop"
+GROUP_MYSQL="mysql"
+
+
+sudo mysql -u root -p"$MYSQL_PASSWORD_ROOT" <<EOF
 CREATE DATABASE $ZABBIX_DB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 CREATE USER '$ZABBIX_USER'@'localhost' IDENTIFIED BY '$ZABBIX_PASSWORD';
 GRANT ALL PRIVILEGES ON $ZABBIX_DB.* TO '$ZABBIX_USER'@'localhost';
@@ -15,22 +32,33 @@ EOF
 
 echo "Banco de dados e usuário zabbix configurado com sucesso!"
 
-zcat "$SQL_SCRIPT_PATH" | mysql --default-character-set=utf8mb4 -u"$ZABBIX_USER" -p"$ZABBIX_PASSWORD" "$ZABBIX_DB"
+echo "Importando banco de dados. Por favor, espere um pouco..."
+sudo zcat "$SQL_SCRIPT_PATH" | mysql --default-character-set=utf8mb4 -u"$ZABBIX_USER" -p"$ZABBIX_PASSWORD" "$ZABBIX_DB"
 
 echo "Esquema SQL do Zabbix importado com sucesso!"
 
-mysql -u root -p"$MYSQL_PASSWORD_ROOT" <<EOF
+sudo mysql -u root -p"$MYSQL_PASSWORD_ROOT" <<EOF
 set global log_bin_trust_function_creators = 0;
 EOF
 
 path_file_zabbix="/etc/zabbix/zabbix_server.conf"
 
-sed -i '/DBPassword/d' "$path_file_zabbix"
+sudo sed -i '/DBPassword/d' "$path_file_zabbix"
 
 line_to_add_zabbix_conf=$(cat <<- EOM
 DBPassword=$ZABBIX_PASSWORD
 EOM
 )
 
-echo "$line_to_add_zabbix_conf" >> "$path_file_zabbix"
+sudo gpasswd -a "$USER_HADOOP" "$GROUP_MYSQL"
+sudo chmod 777 /var/run/mysqld
+
+
+echo "$line_to_add_zabbix_conf" | sudo tee "$path_file_zabbix"
+
+sudo service zabbix-server start
+sudo service zabbix-agent start
+sudo service apache2 start
+
+echo "Servidor Zabbix está pronto para uso. Acesse: localhost/zabbix"
 
