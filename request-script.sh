c786b8575fb0f89517d9e4bc7a905536b93028f0
eric <ericjesus403@gmail.com>
2024-09-18 18:38:52 -0300

    feat: Faz a requisição para todos os slaves no endereço 9864 e reinicia o cluster caso algum deles não responda

---
+++ request-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path_file_workers="$HADOOP_HOME/etc/hadoop/workers"
+stop_hadoop_cluster="$HADOOP_HOME/sbin/stop-all.sh"
+start_hadoop_cluster="$HADOOP_HOME/sbin/start-all.sh"
+
+while IFS= read -p linha; do
+	sleep 200
+	status_code=$(curl -o /dev/null -s -w "%{http_code}" "$linha:9864") 
+	if [[ "$status_code" != 200 ]]; then
+		$stop_hadoop_cluster
+		sleep 5
+		$start_hadoop_cluster
+	fi
+done < "$path_file_workers"
+		
+		
