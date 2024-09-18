e500a404990bccf3c9fc9e9579ffaa4253d34fed
eric <ericjesus403@gmail.com>
2024-09-18 20:08:58 -0300

    fix: Compara o status code com 302 e faz tarefas de acordo com o status code

--- request-script.sh
+++ request-script.sh
@@ -5,14 +5,14 @@ stop_hadoop_cluster="$HADOOP_HOME/sbin/stop-all.sh"
 start_hadoop_cluster="$HADOOP_HOME/sbin/start-all.sh"
 
 while IFS= read linha; do
-	sleep 200
 	status_code=$(curl -o /dev/null -s -w "%{http_code}" "$linha:8042")
         echo $linha	
-	if [[ "$status_code" != 200 ]]; then
+	if [[ "$status_code" != 302 ]]; then
 		$stop_hadoop_cluster
 		sleep 5
 		$start_hadoop_cluster
 	fi
+	sleep 200
 done < "$path_file_workers"
 		
 		
