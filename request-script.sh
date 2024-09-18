d0d1e8318c80cba7a3c2e02bc419f268174a5060
eric <ericjesus403@gmail.com>
2024-09-18 19:40:58 -0300

    fix: Muda a porta de requisição de 9864 para 8042

--- request-script.sh
+++ request-script.sh
@@ -6,7 +6,8 @@ start_hadoop_cluster="$HADOOP_HOME/sbin/start-all.sh"
 
 while IFS= read linha; do
 	sleep 200
-	status_code=$(curl -o /dev/null -s -w "%{http_code}" "$linha:9864") 
+	status_code=$(curl -o /dev/null -s -w "%{http_code}" "$linha:8042")
+        echo $linha	
 	if [[ "$status_code" != 200 ]]; then
 		$stop_hadoop_cluster
 		sleep 5
