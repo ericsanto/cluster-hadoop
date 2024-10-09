653966ee29afb2f45cb4b9b821a275e1be6728ce
eric <ericjesus403@gmail.com>
2024-10-09 09:48:06 -0300

    feat: Cria rotas para as funcionalidades

---
+++ routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"api/server"
+
+	"github.com/labstack/echo/v4"
+)
+
+func ConfigurationsRoutes(e *echo.Echo) {
+
+	e.GET("/api/list-all-containers", server.APIListAllContainerHandler)
+	e.GET("/api/list-exec-containers", server.APIListExecContainerHandler)
+	e.POST("/api/:id/stop-container", server.APIStopContainerHandle)
+	e.POST("/api/:id/start-container", server.APIStartContainerHandle)
+	e.GET("/api/containers/:id/statics", server.APIStatsContainerHandle)
+}
