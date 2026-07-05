777b66fd8d0845c2d4dc376e853662471ca26604
Éric Santos <ericjesus403@gmail.com>
2026-07-05 14:23:33 -0300

    feat: create file config for machine is mac

--- new_cluster/scripts/config-ssh.sh
+++ new_cluster/scripts/config-ssh.sh
@@ -9,3 +9,12 @@ PUBLIC_KEY=$(cat ~/.ssh/id_rsa.pub)
 if ! grep -qxF "$PUBLIC_KEY" ~/.ssh/authorized_keys; then
     echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
 fi
+
+if [[ "$HOSTNAME" == "master" ]] && ! grep -q "^Host MacBookPro$" ~/.ssh/config 2>/dev/null; then
+    cat >> ~/.ssh/config <<EOF
+Host MacBookPro
+    HostName 10.144.161.17
+    User hadoop
+    Port 2222
+EOF
+fi
