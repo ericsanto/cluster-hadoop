c20d98f849fb1f58e82db516688da870b748aab2
Éric Santos <ericjesus403@gmail.com>
2026-07-05 22:31:24 -0300

    feat: add metrics yarn  env

---
+++ new_cluster/scripts/yarn-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+at <<EOF > "$HADOOP_HOME/etc/hadoop/yarn-env.sh"
+
+export YARN_RESOURCEMANAGER_OPTS="$YARN_RESOURCEMANAGER_OPTS \
+-javaagent:/opt/jmx/jmx_prometheus_javaagent.jar=9405:/opt/jmx/resourcemanager.yml"
+
+export YARN_NODEMANAGER_OPTS="$YARN_NODEMANAGER_OPTS \
+-javaagent:/opt/jmx/jmx_prometheus_javaagent.jar=9407:/opt/jmx/nodemanager.yml"
+
+
+EOF
