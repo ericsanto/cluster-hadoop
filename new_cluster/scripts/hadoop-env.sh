a8f6a5f27222d9248e20353be49d3672475f347c
Éric Santos <ericjesus403@gmail.com>
2026-07-08 15:37:23 -0300

    feat: add config for metrics

--- new_cluster/scripts/hadoop-env.sh
+++ new_cluster/scripts/hadoop-env.sh
@@ -15,9 +15,9 @@ export PATH=\$PATH:\$HADOOP_HOME/bin
 export HADOOP_SSH_OPTS="-i ~/.ssh/id_rsa"
 
 export HDFS_NAMENODE_OPTS="$HDFS_NAMENODE_OPTS \
--javaagent:/opt/jmx/jmx_prometheus_javaagent.jar=9404:/opt/jmx/namenode.yml"
+-javaagent:/opt/jmx/jmx_prometheus_javaagent-1.6.0.jar=9404:/opt/jmx/namenode.yml"
 
 export HDFS_DATANODE_OPTS="$HDFS_DATANODE_OPTS \
--javaagent:/opt/jmx/jmx_prometheus_javaagent.jar=9406:/opt/jmx/datanode.yml"
+-javaagent:/opt/jmx/jmx_prometheus_javaagent-1.6.0.jar=9406:/opt/jmx/datanode.yml"
 
 EOF
