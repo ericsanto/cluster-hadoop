3d1675ac381ea7ac0f345c5a5e66084d704bae72
Eric Santos <eric.santos@querodelivery.com>
2026-08-09 21:43:32 -0300

    feat: configure yarn limits for machines

---
+++ cli/backend/usecase/yarn.go
@@ -0,0 +1,106 @@
+package usecase
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/ericsanto/S.H.A.N.K.S/cli/backend/models"
+)
+
+func ConfigureYarnLimits(configCluster models.Config) error {
+
+	yarnMaster, err := configCluster.Cluster.Namenode.ConvertToYarnLimit()
+
+	if err != nil {
+		return fmt.Errorf("erro ao converter o limite de memória do Yarn do Namenode: %v", err)
+	}
+
+	pathPrivateKey, err := getPathPrivateKey()
+	if err != nil {
+		return err
+	}
+
+	validatedYarnMaster, err := validateYarnLimits(configCluster.Cluster.Namenode.IP, configCluster.Cluster.Namenode.User, pathPrivateKey, yarnMaster)
+	if err != nil {
+		return err
+	}
+
+	configCluster.Cluster.Namenode.YarnLimit = *validatedYarnMaster
+
+	yarnWorkers := make([]models.Datanode, len(configCluster.Cluster.Datanodes)-1)
+
+	for _, datanode := range configCluster.Cluster.Datanodes {
+
+		yarnLimit, err := datanode.ConvertToYarnLimit()
+		if err != nil {
+			return fmt.Errorf("erro ao converter o limite de memória do Yarn do Datanode %s: %v", datanode.Name, err)
+		}
+
+		validatedYarnLimit, err := validateYarnLimits(datanode.IP, datanode.User, pathPrivateKey, yarnLimit)
+		if err != nil {
+			return err
+		}
+
+		datanode.YarnLimit = *validatedYarnLimit
+		yarnWorkers = append(yarnWorkers, datanode)
+	}
+
+	if err := insertYarnLimitsToEnvFile(configCluster.Cluster.Namenode, yarnWorkers); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func validateYarnLimits(ip, user, pathPrivateKey string, yarnLimit float64) (*string, error) {
+
+	memory, err := runSSHCommand(ip, "22", user, pathPrivateKey, "free -m | awk 'NR==2{print $2}'")
+	if err != nil {
+		return nil, err
+	}
+
+	intMemory, err := strconv.ParseFloat(strings.TrimSpace(memory), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	if intMemory < yarnLimit {
+		yarnLimit = intMemory * 70 / 100
+		yarnLimitStr := strconv.FormatFloat(yarnLimit, 'f', -1, 64)
+		return &yarnLimitStr, nil
+
+	} else {
+		yarnLimitStr := strconv.FormatFloat(yarnLimit, 'f', -1, 64)
+		return &yarnLimitStr, nil
+	}
+}
+
+func insertYarnLimitsToEnvFile(namenode models.Namenode, datanodes []models.Datanode) error {
+
+	var stringBuffer strings.Builder
+
+	file, err := os.OpenFile(".env", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("erro ao abrir o arquivo .env: %v", err)
+	}
+	defer file.Close()
+
+	stringBuffer.WriteString(fmt.Sprintf("namenode-YARN-LIMIT=%s\n", namenode.YarnLimit))
+
+	for i, datanode := range datanodes {
+
+		fmt.Println("DATANODE ", i)
+		stringBuffer.WriteString(fmt.Sprintf("datanode-%d-YARN-LIMIT=%s\n", i+1, datanode.YarnLimit))
+
+	}
+
+	_, err = file.WriteString(stringBuffer.String())
+	if err != nil {
+		return fmt.Errorf("erro ao escrever no arquivo .env: %v", err)
+	}
+
+	return nil
+}
