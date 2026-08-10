6f736056426e4c2893150ab7c9d17b014ead4bcd
Eric Santos <eric.santos@querodelivery.com>
2026-08-09 21:39:31 -0300

    feat: create function for convert gb in bytes

--- cli/backend/models/datanode.go
+++ cli/backend/models/datanode.go
@@ -1,7 +1,24 @@
 package models
 
+import "strconv"
+
 type Datanode struct {
-	User string `yaml:"user"`
-	Name string `yaml:"name"`
-	IP   string `yaml:"ip"`
+	User       string `yaml:"user"`
+	Name       string `yaml:"name"`
+	IP         string `yaml:"ip"`
+	YarnLimit  string `yaml:"yarn_limit"`
+	SparkLimit string `yaml:"spark_limit"`
+}
+
+func (d *Datanode) ConvertToYarnLimit() (float64, error) {
+
+	intYarnLimit, err := strconv.ParseFloat(d.YarnLimit, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	byteYarnLimit := intYarnLimit * 1000
+
+	return byteYarnLimit, nil
+
 }
--- cli/backend/models/namenode.go
+++ cli/backend/models/namenode.go
@@ -1,7 +1,24 @@
 package models
 
+import "strconv"
+
 type Namenode struct {
-	User string `yaml:"user"`
-	Name string `yaml:"name"`
-	IP   string `yaml:"ip"`
+	User       string `yaml:"user"`
+	Name       string `yaml:"name"`
+	IP         string `yaml:"ip"`
+	YarnLimit  string `yaml:"yarn_limit"`
+	SparkLimit string `yaml:"spark_limit"`
+}
+
+func (n *Namenode) ConvertToYarnLimit() (float64, error) {
+
+	intYarnLimit, err := strconv.ParseFloat(n.YarnLimit, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	byteYarnLimit := intYarnLimit * 1000
+
+	return byteYarnLimit, nil
+
 }
