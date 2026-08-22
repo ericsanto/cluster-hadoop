79ff543772567523555232ddcb5c85dae1dfdd70
Eric Santos <eric.santos@querodelivery.com>
2026-08-22 15:47:38 -0300

    chore: injection root file for execute cli

--- cli/backend/cmd/root.go
+++ cli/backend/cmd/root.go
@@ -1,48 +1,21 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/ericsanto/S.H.A.N.K.S/cli/backend/usecase"
 	"github.com/spf13/cobra"
 )
 
-var cfgFile string
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "start",
-	Short: "Inicia o cluster",
+	Use:   "shanks",
+	Short: "Orquestrador de cluster para processamento distribuído",
 	Long: `S.H.A.N.K.S é uma ferramenta para iniciar um cluster Hadoop em um ambiente distribuído.
 	Ela lê um arquivo de configuração YAML que contém informações sobre o cluster, como os nós e suas credenciais, e 
 	configura os arquivos /etc/hosts de cada nó para que eles possam se comunicar entre si.
 	Exemplo de uso:
   	./start --config /caminho/para/config.yaml`,
 	Run: func(cmd *cobra.Command, args []string) {
-		clusterConfigs, err := usecase.ReadYaml(cfgFile)
-		if err != nil {
-			fmt.Println("nao foi possivel fazer a leitura do arquivo: ", err)
-			return
-		}
-
-		if err := usecase.ConfigHosts(*clusterConfigs); err != nil {
-			fmt.Println("erro ao enviar ssh para as maquinas: ", err)
-			return
-		}
-
-		if err := usecase.ConfigureYarnLimits(*clusterConfigs); err != nil {
-			fmt.Println("erro ao configurar os limites do yarn: ", err)
-			return
-		}
-
-		if err := usecase.StartCluster(*clusterConfigs); err != nil {
-			fmt.Println("erro ao startar containers: ", err)
-			return
-		}
 	},
 }
 
@@ -53,16 +26,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "caminho para o arquivo yaml")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
